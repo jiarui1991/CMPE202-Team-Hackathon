efd4598594c1785267952b5480af3aa84aa39edc
euglin1130 <caoqijia1130@gmail.com>
2017-04-10 23:08:50 -0700

    Add Button Class

---
+++ MonitorButton.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MonitorButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MonitorButton extends Button
+{
+     public MonitorButton(){
+        GreenfootImage image = new GreenfootImage(100, 50);
+        image.drawRect(0, 0, 50, 30);
+        setImage(image);
+    }
+    /**
+     * Act - do whatever the MonitorButton wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
